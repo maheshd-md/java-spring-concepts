528bf2d11eeb2e7aecb4ea8d3473ec57023d3d30
Mahesh Dhavalshankh <maheshdhavalshankh@Maheshs-MacBook-Air.local>
2022-06-17 12:34:32 +0530

    Callable examples added

---
+++ executer-service-demo/src/com/maheshd/executerservice/CallableTask.java
@@ -0,0 +1,22 @@
+package com.maheshd.executerservice;
+
+import java.util.concurrent.Callable;
+
+public class CallableTask implements Callable<Integer> {
+
+	private Integer input;
+	
+	public CallableTask(Integer input) {
+		super();
+		this.input = input;
+	}
+
+	@Override
+	public Integer call() throws Exception {
+		Integer sum = 0;
+		for(int i=1; i<=input; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+}
--- executer-service-demo/src/com/maheshd/executerservice/MainClass.java
+++ executer-service-demo/src/com/maheshd/executerservice/MainClass.java
@@ -3,10 +3,14 @@
  */
 package com.maheshd.executerservice;
 
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Mahesh
@@ -30,12 +34,12 @@ public class MainClass {
 		printSeparatorAfterExecutorCompletion(fixedThreadPoolExecutorService);
 
 		/*
-		 * ---Other methods of ExecutorService:
-		 * 1. executorService.shutdown() - initiate shutdown 
-		 * 2. executorService.isShutdown() - returns true if executorService is shutdown 
-		 * 3. executorService.isTerminated() - returns true if all the tasks are completed 
-		 * 4. executorService.awaitTermination(long timout, TimeUnit timeUnit) - blocks current thread execution 
-		 * until timout or termiation of executorService
+		 * ---Other methods of ExecutorService: 1. executorService.shutdown() - initiate
+		 * shutdown 2. executorService.isShutdown() - returns true if executorService is
+		 * shutdown 3. executorService.isTerminated() - returns true if all the tasks
+		 * are completed 4. executorService.awaitTermination(long timout, TimeUnit
+		 * timeUnit) - blocks current thread execution until timout or termiation of
+		 * executorService
 		 */
 
 		System.out.println("2. Cached Thread Pool Executor Service: ");
@@ -95,6 +99,71 @@ public class MainClass {
 		}
 
 		printSeparatorAfterExecutorCompletion(singleThreadExecutor);
+
+		/*
+		 * When we want return value from thread, implement callable interface and
+		 * override call() method.
+		 */
+		CallableTask callableTask1 = new CallableTask(10);
+		CallableTask callableTask2 = new CallableTask(20);
+		CallableTask callableTask3 = new CallableTask(30);
+		ExecutorService callableService = Executors.newSingleThreadExecutor();
+
+		Future<Integer> future1 = callableService.submit(callableTask1);
+		try {
+			/*
+			 * Future.get() method blocks the execution until get the return value from the
+			 * callable task
+			 */
+			Integer sum = future1.get();
+			System.out.println(sum);
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		Future<Integer> future2 = callableService.submit(callableTask2);
+		try {
+			/*
+			 * Future.get(long timeout, TimeUnit timeUnit) with timeout parameter blocks the
+			 * execution for a given timeout, if value is not returned in given time throws
+			 * TimeoutException
+			 */
+			Integer sum = future2.get(1, TimeUnit.MICROSECONDS);
+			System.out.println(sum);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			e.printStackTrace();
+		}
+
+		Future<Integer> future3 = callableService.submit(callableTask3);
+
+		/*
+		 * Future.cancel(boolean mayInterruptIfRunning) method cancels the execution of
+		 * the callable task, if task is not started yet.
+		 * 
+		 * If task is already started and mayInterruptIfRunning = true, it interrupts
+		 * the task and if mayInterruptIfRunning = false, it completes the execution
+		 */
+		future3.cancel(false);
+
+		/*
+		 * Future.idDone() checks whether callable task is completed. Completion may be
+		 * due to normal termination, an exception, or cancellation -- in all of these
+		 * cases, this method will return true.
+		 */
+		if (future3.isDone()) {
+			try {
+				System.out.println("Callable task 3 is done!");
+				// Calling get() method on cancelled future, throws CancellationException
+				Integer sum = future3.get();
+			} catch (InterruptedException | ExecutionException | CancellationException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Future.isCancelled() checks whether callable task is cancelled
+		if (future3.isCancelled()) {
+			System.out.println("Callable task 3 is cancelled!");
+		}
 	}
 
 	private static void printSeparatorAfterExecutorCompletion(ExecutorService executorService) {
