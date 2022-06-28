f6d702323fb116976db9e38e0076701cc911694f
Mahesh Dhavalshankh <dhavalshankh.md@gmail.com>
2022-06-29 01:27:25 +0530

    Updated java 8 features class

--- java8features/src/java8features/MainClass.java
+++ java8features/src/java8features/MainClass.java
@@ -51,7 +51,8 @@ public class MainClass {
 		list.stream().sorted(Comparator.comparingInt(Employee::getSalary).reversed()).forEach(System.out::println);
 
 		// Print the distinct salaries
-		// For the method distict(), hashCode & equals methods should be implemented by the class
+		// For the method distict(), hashCode & equals methods should be implemented by
+		// the class
 		list.stream().map(Employee::getSalary).distinct().forEach(System.out::println);
 		System.out.println("----------------------------------------------------------------------");
 
@@ -86,22 +87,24 @@ public class MainClass {
 		}));
 		System.out.println(emp.get());
 		System.out.println("----------------------------------------------------------------------");
-		
+
 		// Find the average salary
 		OptionalDouble avgSalary = list.stream().mapToInt(e -> e.getSalary().intValue()).average();
 		System.out.println(avgSalary.getAsDouble());
 		System.out.println("----------------------------------------------------------------------");
-		
+
 		// Find the 2nd largest salary
-		Optional<Integer> secondHighestSalary = list.stream().map(Employee::getSalary).distinct().sorted().skip(1).findFirst();		
+		Optional<Integer> secondHighestSalary = list.stream().map(Employee::getSalary).distinct().sorted().skip(1)
+				.findFirst();
 		System.out.println("2nd highest salary: " + secondHighestSalary.get());
 		System.out.println("----------------------------------------------------------------------");
 
-		// Find the employee with 2nd largest salary, if salary is same print print employee whose name comes first in alphabetical order
+		// Find the employee with 2nd largest salary, if salary is same print print
+		// employee whose name comes first in alphabetical order
 		emp = list.stream().sorted(Comparator.comparingInt(Employee::getSalary).reversed()).filter(e -> {
 			return e.getSalary() < list.stream().max(Comparator.comparingInt(Employee::getSalary)).get().getSalary();
 		}).sorted(Comparator.comparing(Employee::getName)).findFirst();
-		
+
 		System.out.println("Employee with 2nd highest salary: " + emp.get());
 		System.out.println("----------------------------------------------------------------------");
 
@@ -112,25 +115,24 @@ public class MainClass {
 		// Example of parallelStream with order
 		list.parallelStream().forEachOrdered(System.out::println);
 		System.out.println("----------------------------------------------------------------------");
-		
-		
+
 		// Group by skill to map of list
 		Map<String, List<Employee>> groupBySkills = list.stream().collect(Collectors.groupingBy(e -> e.getSkill()));
 		System.out.println(groupBySkills);
 		System.out.println("----------------------------------------------------------------------");
 
 		// Group by skill to map of set
-		Map<String, Set<Employee>> groupBySkillsUnique = list.stream().collect(Collectors.groupingBy(Employee::getSkill, Collectors.toSet()));
+		Map<String, Set<Employee>> groupBySkillsUnique = list.stream()
+				.collect(Collectors.groupingBy(Employee::getSkill, Collectors.toSet()));
 		System.out.println(groupBySkillsUnique);
 		System.out.println("----------------------------------------------------------------------");
 
 		// Group by skill to treemap of list (sorted)
-		Map<Integer, Set<Employee>> groupBySalarySorted = list.stream().collect(Collectors.groupingBy(e -> e.getSalary(), TreeMap::new, Collectors.toSet()));
+		Map<Integer, Set<Employee>> groupBySalarySorted = list.stream()
+				.collect(Collectors.groupingBy(e -> e.getSalary(), TreeMap::new, Collectors.toSet()));
 		System.out.println(groupBySalarySorted);
 		System.out.println("----------------------------------------------------------------------");
-		
-		
-		
+
 	}
 
 }
